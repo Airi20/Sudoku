85ea89346b1b9fc0392bd6720cfd3496e117db41
Airi <aiai9004432221@gmail.com>
2025-07-15 20:20:28 +0900

    2

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,26 @@ import SudokuBoard from "./components/SudokuBoard";
 import "./App.css";
 
 export default function App() {
-  const [grid, setGrid] = useState([
-    [5, 3, null, null, 7, null, null, null, null],
-    [6, null, null, 1, 9, 5, null, null, null],
-    [null, 9, 8, null, null, null, null, 6, null],
-    [8, null, null, null, 6, null, null, null, 3],
-    [4, null, null, 8, null, 3, null, null, 1],
-    [7, null, null, null, 2, null, null, null, 6],
-    [null, 6, null, null, null, null, 2, 8, null],
-    [null, null, null, 4, 1, 9, null, null, 5],
-    [null, null, null, null, 8, null, null, 7, 9],
-  ]);
+  const initialGrid = [
+  [5, 3, null, null, 7, null, null, null, null],
+  [6, null, null, 1, 9, 5, null, null, null],
+  [null, 9, 8, null, null, null, null, 6, null],
+  [8, null, null, null, 6, null, null, null, 3],
+  [4, null, null, 8, null, 3, null, null, 1],
+  [7, null, null, null, 2, null, null, null, 6],
+  [null, 6, null, null, null, null, 2, 8, null],
+  [null, null, null, 4, 1, 9, null, null, 5],
+  [null, null, null, null, 8, null, null, 7, 9],
+];
+
+const [grid, setGrid] = useState(initialGrid);
 
   const [message, setMessage] = useState("");
   const [showComment, setShowComment] = useState(false);
 
   const fixedCells = initialGrid.map(row => row.map(cell => cell !== null));
+
+
 
 
   const solution = [
